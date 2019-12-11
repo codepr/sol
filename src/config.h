ad3aae4b73514a2f1d8ce3eed482576b5f964c77
Andrea <a.g.baldan@gmail.com>
2019-12-11 18:34:32 +0100

    Bump version

--- src/config.h
+++ src/config.h
@@ -34,7 +34,7 @@
 
 // Default parameters
 
-#define VERSION                     "0.9.2"
+#define VERSION                     "0.9.3"
 #define DEFAULT_SOCKET_FAMILY       INET
 #define DEFAULT_LOG_LEVEL           DEBUG
 #define DEFAULT_LOG_PATH            "/tmp/sol.log"
